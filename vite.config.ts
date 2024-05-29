12aa8180558232e5fd5d1da92deda695fdd2aeb4
hhuabin <hhuabin@qq.com>
2024-05-29 21:49:05 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	base: './',
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': new URL('./src', import.meta.url).pathname,
+		}
+	},
+	server: {
+		port: 3000,
+	},
 })
