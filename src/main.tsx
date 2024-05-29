cf9f5442031459622c3f81493adff09e6982f82f
hhuabin <hhuabin@qq.com>
2024-05-29 22:05:01 +0800

    tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 
 import store from './store/store'
 
+import './styles/tailwind.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
