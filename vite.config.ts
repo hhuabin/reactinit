b9384942b33868b870106e8690cd60f7baf0fe8c
hhuabin <hhhuabin@gmail.com>
2024-05-30 16:02:30 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,21 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'js/[name].js',
+				chunkFileNames: 'js/[name]-[hash].js',
+				assetFileNames(chunkInfo) {
+					const imgExts = ['.jpg', '.png', '.jpeg', '.webp', '.svg', '.gif', '.ico']
+					if (chunkInfo.name?.endsWith('.css')) {
+						return 'css/[name]-[hash].css'
+					} else if (imgExts.some(ext => chunkInfo.name?.endsWith(ext))) {
+						return 'images/[name]-[hash][extname]'
+					}
+					return 'assets/[name]-[hash][extname]'
+				},
+			}
+		},
+	},
 })
