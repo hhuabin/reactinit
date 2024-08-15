239a9d33ebe6a9b144418d1f58732c63ac4915de
hhuabin <hhhuabin@gmail.com>
2024-08-15 17:09:16 +0800

    performance

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,34 @@ const App: React.FC = () => {
 	}))
 
 	useEffect(() => {
-		console.log("import.meta.env", import.meta)
-	})
+		// 性能监控
+		console.log("import.meta.env", import.meta.env.MODE)
+		if (import.meta.env.MODE === 'production') return
+
+		// LCP监听
+		const observer = new PerformanceObserver((entryList) => {
+			const entries = entryList.getEntries()
+			const lcpEntry = entries[entries.length - 1]
+			console.log("lcpEntry", lcpEntry);
+			
+			console.log('LCP:' + lcpEntry.startTime + 'ms')
+		});
+		observer.observe({ type: 'largest-contentful-paint', buffered: true })
+
+		// Onload Event
+		setTimeout(() => {
+			const navigationEntries = performance.getEntriesByType('navigation')
+			const navTiming = navigationEntries[0] as PerformanceNavigationTiming      // 通常只有一个导航条目
+			console.log(navTiming)
+			console.log('DOMContentLoaded:', navTiming.domContentLoadedEventEnd - navTiming.startTime + 'ms')
+			console.log('OnLoadTime:', navTiming.loadEventEnd - navTiming.startTime + 'ms')
+		}, 100)
+
+		return () => {
+			// 移除PerformanceObserver监听
+			observer.disconnect();
+		}
+	}, [])
 
 	return (
 		<div id="app">
