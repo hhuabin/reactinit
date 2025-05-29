4a8d3a39ca8ed485472ba84c63dba066c3dfd881
hhuabin <hhhuabin@gmail.com>
2025-05-29 09:24:51 +0800

    update useProjectAutoUpdate

--- src/hooks/useProjectAutoUpdate.ts
+++ src/hooks/useProjectAutoUpdate.ts
@@ -6,36 +6,45 @@ import { useRef, useEffect } from "react"
  * index.html需要增加 version 和 timestamp 的 meta 标签
  * vite.config.ts 需要配置自动修改 version meta 标签值为 package.json 的 version
  * vite.config.ts 需要配置自动修改 timestamp meta 标签值为当前时间戳
- * @param projectLink 项目部署地址，默认为域名根路径
- * @param realTime 是否实时检查，默认为 false
+ * @param { string } projectLink 项目部署于域名下的路径，默认为域名根路径/；如果项目部署于子路径，则需要填写子路径，如 /project/
+ * @param { boolean } intervalRefresh 是否定时轮询检查更新，默认为 false
  */
-const useProjectAutoUpdate = (projectLink = '/', realTime = false) => {
+const useProjectAutoUpdate = (projectLink = '/', intervalRefresh = false) => {
 
     // <meta name="version" content="__VERSION__" />
     const versionAndtimestampRegex = /<meta\s+name="(version|timestamp)"\s+content="([^"]+)"\s*\/?>/gi
     const projectVersion = useRef("")
     const projectBuildTime = useRef("")
-    const REALTIME_DURATION = 3000
+    const intervalRefreshTimer = useRef<NodeJS.Timeout | null>(null)
+    const REFRESH_INTERVAL = 60000      // 一分钟轮询检查更新一次
 
     useEffect(() => {
-        if (import.meta.env.MODE !== 'production') return
-        getVersionAndTimeFromLocalHtmlMeta()
-        startCheckRefresh()
+        // if (import.meta.env.MODE !== 'production') return
+        getVersionAndTimeFromLocalHtmlMetaAndStartRefresh()
+
+        return () => {
+            clearTntervalRefreshTimer()
+        }
     }, [])
 
     /**
      * 获取当前（包括本地存储）页面的版本号和编译时间
      */
-    const getVersionAndTimeFromLocalHtmlMeta = () => {
+    const getVersionAndTimeFromLocalHtmlMetaAndStartRefresh = () => {
         const versionMeta = document.querySelector('meta[name="version"]')
         const buildTimeMeta = document.querySelector('meta[name="timestamp"]')
         projectVersion.current = versionMeta?.getAttribute('content') || ""
         projectBuildTime.current = buildTimeMeta?.getAttribute('content') || ""
+        if (!projectVersion.current || !projectBuildTime.current) {
+            console.warn("useProjectAutoUpdate：当前页面未找到版本信息，请检查是否已配置版本信息；检测项目更新失败，请稍后重试")
+            return
+        }
+        startCheckRefresh()
     }
 
     /**
      * 提取链接（项目部署地址）中的版本号和构建打包时间
-     * @param { string } link 项目部署地址
+     * @param { string } link 项目部署于域名下的路径
      * @returns { Promise<[string, string]> } [版本号, 时间戳]
      */
     const extractMetaFromLink = (link = projectLink): Promise<[string, string]> => {
@@ -54,18 +63,26 @@ const useProjectAutoUpdate = (projectLink = '/', realTime = false) => {
                         result[1] = match[2]
                     }
                 }
-                return Promise.resolve(result)
+                if (result[0] && result[1]) {
+                    return Promise.resolve(result)
+                } else {
+                    return Promise.reject("useProjectAutoUpdate：远程页面未找到版本信息，请检查是否已配置版本信息；检测项目更新失败，请稍后重试")
+                }
             })
             .catch(error => {
+                console.warn(error)
                 return Promise.reject(error)
             })
     }
 
     // 检测项目是否需要更新
-    const isProjectNeedUpdate = async (): Promise<boolean> => {
+    const checkProjectUpdate = async () => {
         const [version, buildTime] = await extractMetaFromLink(projectLink)
         let isNeedUpdate = false
-        // 版本号和构建打包时间都不同，则需要更新
+        /**
+         * 更新判定标准，可根据需要修改
+         * 版本号和构建打包时间都不同，则需要更新
+         */
         if (version !== projectVersion.current && buildTime !== projectBuildTime.current) {
             isNeedUpdate = true
             console.warn(`current version: ${projectVersion.current}, Latest version: ${version}`)
@@ -74,26 +91,38 @@ const useProjectAutoUpdate = (projectLink = '/', realTime = false) => {
         // 避免多次弹窗更新
         projectVersion.current = version
         projectBuildTime.current = buildTime
-
-        return isNeedUpdate
+        if (isNeedUpdate) {
+            // 避免多次弹窗更新
+            clearTntervalRefreshTimer()
+            // 弹出用户更新的弹窗
+            if (window.confirm('检测到更新，是否刷新页面？')) {
+                window.location.reload()
+            }
+        }
     }
 
-    // 实时检查项目是否需要更新
+    // 初始和轮询检查项目更新
     const startCheckRefresh = () => {
-        setTimeout(async () => {
-            const willUpdate = await isProjectNeedUpdate()
-            if (willUpdate) {
-                // 弹出用户更新的弹窗
-                if (window.confirm('检测到更新，是否刷新页面？')) {
-                    window.location.reload()
-                } else {
-                    // 用手取消更新，不再轮询更新
-                    return
-                }
-            }
-            // 实时检查项目是否需要更新
-            realTime && startCheckRefresh()
-        }, REALTIME_DURATION)
+        // 初始化 3s 后检查一次更新，给用户反应时间
+        setTimeout(() => {
+            checkProjectUpdate()
+        }, 3000)
+
+        // 循环检查更新
+        if (intervalRefresh) {
+            clearTntervalRefreshTimer()
+            intervalRefreshTimer.current = setInterval(() => {
+                checkProjectUpdate()
+            }, REFRESH_INTERVAL)
+        }
+    }
+
+    // 清除循环检查更新定时器
+    const clearTntervalRefreshTimer = () => {
+        if (intervalRefreshTimer.current) {
+            clearInterval(intervalRefreshTimer.current)
+            intervalRefreshTimer.current = null
+        }
     }
 }
 
