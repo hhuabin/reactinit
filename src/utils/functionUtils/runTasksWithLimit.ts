f155fd2f00b037534d8e919e6d1e8f289d2f428d
hhuabin <hhhuabin@gmail.com>
2025-07-21 12:25:42 +0800

    runTasksWithLimit

--- src/utils/functionUtils/runTasksWithLimit.ts
+++ src/utils/functionUtils/runTasksWithLimit.ts
@@ -1,21 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-type Task<T extends any[]> = () => Promise<T[number]>
+export type TaskFn<T = any> = () => Promise<T>
 
-type TaskResult<T> =
+export type TaskResult<T> =
   | { status: 'fulfilled'; value: T }         // fulfilled：成功返回
   | { status: 'rejected'; reason: any };      // rejected：失败返回
 
 /**
  * @description 任务并发控制函数，任务数控制在 limit 以内，参考 Promise.all() 使用
- * @param { Task<T>[] } tasks Promise 任务列表
+ * 泛型 T 定义的是参数 tasks 的类型，因为我们需要从 tasks 的类型推导出结果类型，故而需要定义 tasks 的泛型
+ * @param { TaskFn<any>[] } tasks Promise 任务列表
  * @param { number } limit 并发任务数量
  * @returns 全部任务成功时按任务顺序返回所有结果，否则返回第一个失败的错误
  * 该情况 .then() 成功 / 失败状态和 .catch() 都可能会执行
  */
-const runTasksWithLimitFailFast = <T extends any[]>(tasks: Task<T>[], limit: number): Promise<T> => (
-    new Promise((resolve, reject) => {
-        const results: T = [] as unknown as T
+export const runTasksWithLimitFailFast = <T extends (() => Promise<any>)[]>(
+    // 保持传入参数的“元组精度”并允许 as const 修饰的只读数组被正确推导，使用 tasks: T 的话，结果会被推导为宽泛数组
+    tasks: readonly [...T],
+    limit: number,
+): Promise<{[K in keyof T]: T[K] extends () => Promise<infer R> ? R : never}> => {
+    return new Promise((resolve, reject) => {
+        const results = [] as {[K in keyof T]: T[K] extends () => Promise<infer R> ? R : never}
         let currentIndex = 0
         let running = 0
         let finished = 0
@@ -55,18 +60,23 @@ const runTasksWithLimitFailFast = <T extends any[]>(tasks: Task<T>[], limit: num
 
         run()
     })
-)
+}
 
 /**
  * @description 任务并发控制函数，任务数控制在 limit 以内，参考 Promise.allSettled() 使用
- * @param { Task<T>[] } tasks Promise 任务列表
+ * 泛型 T 定义的是参数 tasks 的类型，因为我们需要从 tasks 的类型推导出结果类型，故而需要定义 tasks 的泛型
+ * @param { TaskFn<any>[] } tasks Promise 任务列表
  * @param { number } limit 并发任务数量
  * @returns 无论成功失败，全部执行完成后按任务顺序返回状态和结果
  * 该情况只有 .then() 成功状态会执行，故而给予了 status 去判断成功 / 失败状态
  */
-const runTasksWithLimitSettled = <T extends any[]>(tasks: Task<T>[], limit: number): Promise<TaskResult<keyof T>[]> => {
+export const runTasksWithLimitSettled = <T extends (() => Promise<any>)[]>(
+    // 保持传入参数的“元组精度”并允许 as const 修饰的只读数组被正确推导，使用 tasks: T 的话，结果会被推导为宽泛数组
+    tasks: readonly [...T],
+    limit: number,
+): Promise<{[K in keyof T]: T[K] extends () => Promise<infer R> ? TaskResult<R> : never}> => {
     return new Promise((resolve, reject) => {
-        const results: TaskResult<keyof T>[] = new Array(tasks.length)
+        const results = [] as {[K in keyof T]: T[K] extends () => Promise<infer R> ? TaskResult<R> : never}
         let currentIndex = 0
         let running = 0
         let finished = 0
@@ -109,29 +119,37 @@ const runTasksWithLimitSettled = <T extends any[]>(tasks: Task<T>[], limit: numb
 }
 
 /* const sleep = (id: number, ms: number) => {
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<number>((resolve, reject) => {
         setTimeout(() => {
             console.log(`Task ${id} done`)
-            resolve('任务失败')
+            resolve(id)
         }, ms)
     })
 }
 
 const tasks = [
-    () => sleep(1, 1000),
-    () => sleep(2, 500),
-    () => sleep(3, 300),
-    () => sleep(4, 700),
-    () => sleep(5, 400),
+    () => sleep(1, 1000), () => sleep(2, 500), () => sleep(3, 300),
+    () => sleep(4, 700), () => sleep(5, 400),
 ]
 
-runTasksWithLimitFailFast<[string]>(tasks, 2)
-.then(results => console.log(results[0]))
+// 不定义泛型也可自行推导 results 类型
+runTasksWithLimitFailFast([
+    () => new Promise<string>(resolve => resolve('task 0 success')),
+    () => new Promise<number>(resolve => resolve(1001)),
+], 2)
+.then(results => {
+    results.forEach(result => {
+        console.log(result)
+    })
+})
 .catch(err => console.log(err))
 
-runTasksWithLimitSettled<[string]>(tasks, 2)
+// 也可自定义结果类型
+runTasksWithLimitSettled<Array<() => Promise<number>>>(tasks, 2)
 .then(results => {
     if (results[0].status === 'fulfilled') {
-        console.log(results[0].value)
+        results[0].value
+    } else if (results[0].status === 'rejected') {
+        results[0].reason
     }
 }) */
